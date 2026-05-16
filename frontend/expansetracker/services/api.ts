e78a4079805290c643d0d059b87abd41a752b843
AbuShahma022 <abushahma1014@gmail.com>
2026-05-16 11:24:33 +0600

    url bug fix in fronend

--- frontend/expansetracker/services/api.ts
+++ frontend/expansetracker/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "https://expense-tracker-817j.onrender.com//api/v1",
+  baseURL: "https://expense-tracker-817j.onrender.com/api/v1",
   withCredentials: true,
 })
